a4afef3938ecffc361d3f2b954c4ea678793e036
Bryan Kroesbeek <BryanK.B96@outlook.com>
2016-02-15 12:49:20 +0100

    Project
    Added project to Repo

---
+++ SpeedoMeter/Loop.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace SpeedoMeter {
+    class Loop {
+        private DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Background);
+        private RotateTransform RT = new RotateTransform();
+        private Random rand = new Random();
+        private Stopwatch watch = new Stopwatch();
+
+        private bool accelerating = false, braking = false;
+
+        private int count = 0; //currentPassedSeconds
+        private double count2 = 0; //ccurentPassedMilliSeconds
+
+        private double speedInKM = 0;
+        private const double MAXSPEED = 351;
+        private int maxAngle = 260;
+        private int currentRelativeAngle;
+
+
+        private double acceleration, deceleration = 1.5;
+        private double brakeForce = 17.5;
+
+        private void play(Object O, EventArgs e) {
+            textBox.Content = count2.ToString("0.0");
+
+            speedLabel.Content = speed.ToString("000");
+
+            //acceleration = (0.025 + (maxSpeed - speed) * (maxAcceleration / maxSpeed)) * 3.6;
+
+            acceleration = (2500 / (speed + 116.84255) - 5.21666) * 3.6;
+
+            if (accelerating) {
+                speed += acceleration / 31;
+            } else {
+                speed -= (deceleration + (speed / maxSpeed * 7.5)) / 31;
+            }
+
+
+            if (braking) {
+                brake();
+            }
+
+            if (speed <= 80) {
+                currentRelAngle = (int)Math.Ceiling(speed / 0.72);
+            } else if (speed > 80 && speed <= 200) {
+                currentRelAngle = 48 + (int)Math.Ceiling(speed / 1.26);
+            } else if (speed > 200) {
+                currentRelAngle = 127 + (int)Math.Ceiling(speed / 2.50);
+            }
+
+            if (speed > maxSpeed) {
+                speed -= rand.Next(0, 2);
+            } else if (speed < 0) {
+                speed = 0;
+            }
+
+            if (currentRelAngle > maxAngle) {
+                currentRelAngle = maxAngle;
+            }
+
+            if (speed > 160 && !braking) {
+                currentRelAngle += rand.Next(-1, 1);
+            }
+
+            transform.Angle = currentRelAngle;
+            counting();
+        }
+
+        public double Speed { get; set; }
+    }
+}
---
+++ SpeedoMeter/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace SpeedoMeter {
+    /**
+    *CREATED BY BRYAN KROESBEEK
+    *EDITS MAY BE MADE, BUT ONLY IF THE ORIGINAL CREATOR IS PROPERLY CREDITED
+    */
+    public partial class MainWindow : Window {
+        private DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Background);
+        private RotateTransform RT = new RotateTransform();
+        private Random rand = new Random();
+
+        private bool accelerating = false, braking = false;
+
+        private int quarterSeconds = 0; 
+        private double seconds = 0;
+
+        private double speedInKM = 0;
+        private const double MAXSPEED = 999;
+        private const int MAXANGLE = 260;
+        private int currentRelativeAngle;
+
+
+        private double acceleration = 1.5;
+        private const double DECELERATION = 1.5;
+        private const double BRAKEFORCE = 17.5; // in m/s²
+
+        private const double MPSTOKMH = 3.6; // meters per seconds to kilometers per hour
+
+        public MainWindow() { 
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize() {
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 16);
+            timer.Tick += play;
+            timer.Start();
+        }
+
+        private void accelerateButton_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            accelerating = true;
+        }
+        private void accelerateButton_PreviewMouseUp(object sender, MouseButtonEventArgs e) {
+            accelerating = false;
+        }
+        private void brakeButton_PreviewMouseDown(object sender, MouseButtonEventArgs e) {
+            braking = true;
+        }
+        private void brakeButton_PreviewMouseUp(object sender, MouseButtonEventArgs e) {
+            braking = false;
+        }
+
+        private void play(Object O, EventArgs e) {
+
+            textBox.Content = seconds.ToString("0.0");
+            speedLabel.Content = speedInKM.ToString("000");
+
+            accelerate();
+
+            if (braking) {
+                brake();
+            }
+
+            keepSpeedBelowMaxLevel();
+
+            changeAngle();
+
+            countSeconds();
+        }
+
+        private void keepSpeedBelowMaxLevel() {
+            //reduce the current speed with a value of 1 or 0
+            if (speedInKM > MAXSPEED) 
+                speedInKM -= rand.Next(0, 2);
+            else if (speedInKM < 0) 
+                speedInKM = 0;
+            
+        }
+
+        private void changeAngle() {
+            //change the scale with the corresponding speed
+            if (speedInKM <= 80)
+                currentRelativeAngle = (int)Math.Ceiling(speedInKM / 0.72);
+            else if (speedInKM > 80 && speedInKM <= 200)
+                currentRelativeAngle = 48 + (int)Math.Ceiling(speedInKM / 1.26);
+            else if (speedInKM > 200)
+                currentRelativeAngle = 127 + (int)Math.Ceiling(speedInKM / 2.50);
+           
+
+            if (currentRelativeAngle > MAXANGLE)
+                currentRelativeAngle = MAXANGLE;
+
+            //make the needle wiggle a little bit
+            if (speedInKM > 160 && !braking)
+                currentRelativeAngle += rand.Next(-1, 1);
+
+            transform.Angle = currentRelativeAngle;
+        }
+
+        private void accelerate() {
+            //calculation made in geogebra
+            acceleration = (2500 / (speedInKM + 116.84255) - 5.21666) * MPSTOKMH;
+            //
+
+            if (accelerating)
+                speedInKM += acceleration / 31;
+            else
+                //when going faster the speed will decrease faster
+                speedInKM -= (DECELERATION + (speedInKM / MAXSPEED * 7.5)) / 31;
+        }
+
+        private void brake() {
+            speedInKM -= BRAKEFORCE * MPSTOKMH / 31;
+            if (speedInKM < 0) {
+                speedInKM = 0;
+            }
+        }
+
+        private void countSeconds() {
+            quarterSeconds++;
+            if (quarterSeconds >= 4) {
+                seconds += 0.1;
+                quarterSeconds = 0;
+            }
+        }
+    }
+}
